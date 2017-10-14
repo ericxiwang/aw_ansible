04c8135774645770100cfa2cc451583f5d973c17
fredriksson <gowest.wang@gmail.com>
2017-10-13 23:12:38 -0700

    Update inst_opencv32.sh

--- inst_opencv32.sh
+++ inst_opencv32.sh
@@ -1,2 +1,8 @@
 apt-get -y install git  unzip
 apt-get -y install libopencv-dev build-essential cmake libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
+
+
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+make -j$(nproc)
+make install
