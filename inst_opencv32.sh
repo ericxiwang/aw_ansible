283a110c61b8997b9e5e30de80515ce9e1665099
fredriksson <gowest.wang@gmail.com>
2017-10-13 23:14:25 -0700

    Update inst_opencv32.sh

--- inst_opencv32.sh
+++ inst_opencv32.sh
@@ -6,3 +6,8 @@ apt-get -y install libopencv-dev build-essential cmake libgtk2.0-dev pkg-config 
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make -j$(nproc)
 make install
+
+
+/bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+ldconfig
+pkg-config --modversion opencv
